e36abe75f3ed437c0dca19d59cf9e5c6c603d868
Gaurav Menghani <gauravm@fb.com>
2014-12-31 14:38:42 +0530

    Getting started

---
+++ SnappyCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnappyCam
+//
+//  Created by Gaurav Menghani on 12/31/14.
+//  Copyright (c) 2014 Gaurav Menghani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnappyCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnappyCam
+//
+//  Created by Gaurav Menghani on 12/31/14.
+//  Copyright (c) 2014 Gaurav Menghani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
