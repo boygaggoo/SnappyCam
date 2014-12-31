1af399ae16bdd9921b3c18eb0b5611ed47f1fc9c
Gaurav Menghani <gauravm@fb.com>
2014-12-31 16:09:45 +0530

    Able to pick photos

--- SnappyCam/AppDelegate.m
+++ SnappyCam/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MenuViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    MenuViewController* mvc = [[MenuViewController alloc] init];
+    UINavigationController* nvc = [[UINavigationController alloc] initWithRootViewController:mvc];
+    self.window.rootViewController = nvc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
